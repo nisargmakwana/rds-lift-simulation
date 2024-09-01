a211e9a1982fea0f67dee87c88664c6a954aed02
nisargmakwana <nisargmakwana22@gmail.com>
2024-09-01 23:03:21 +0530

    add initial setup and css

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const floorLiftInputEl = document.querySelector(".grid-container");
+const simulateBtnEl = document.querySelector(".simulate-btn");
+
+simulateBtnEl.addEventListener("click", () => {
+	floorLiftInputEl.classList.add("hidden");
+});
