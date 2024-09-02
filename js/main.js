91bf2cf49ebd4c98d47a317f0a460337a80061ad
nisargmakwana <nisargmakwana22@gmail.com>
2024-09-03 01:11:01 +0530

    refractored rendering

--- js/main.js
+++ js/main.js
@@ -1,77 +1,77 @@
-// INPUT VIEW
-const inputViewEl = document.querySelector(".input-view");
+// selecting all elements
+
+const floorInputEl = document.getElementById("floor-input");
+const liftInputEl = document.getElementById("lift-input");
 const simulateBtnEl = document.querySelector(".simulate-btn");
-const floorInputEl = document.querySelector("#floor-input");
-const liftInputEl = document.querySelector("#lift-input");
 const appViewEl = document.querySelector(".app-container");
+const inputViewEl = document.querySelector(".input-view");
 
-const floorAndLiftState = {
-	floorCount: 0,
-	liftCount: 0,
-	floorContent1: "",
-};
-const liftContent = function (liftCount) {
-	let string = "";
-	for (let i = 0; i < liftCount; i++) {
-		string += '<div class="lift">lift</div>';
+// Setting up input state, lifts, and rendering
+
+const inputStatus = {};
+const lifts = [];
+function setInitialStateAndRender() {
+	inputStatus.floorCount = floorInputEl.value;
+	inputStatus.liftCount = liftInputEl.value;
+	for (let i = 0; i < inputStatus.liftCount; i++) {
+		lifts.push({ index: i + 1, position: 1, busy: false });
 	}
-	return string;
-};
 
-const floorContentLast = `<div class="floor-element">
-			<div>
-				<button class="btn-down">Down</button>
-				<hr>
-			</div>
-			<div class="floor-section">
-			</div>
-		</div>`;
-const floorContent = `<div class="floor-element">
-		<div>
-			<button class="btn-up">Up</button>
-			<button class="btn-down">Down</button>
-			<hr>
-		</div>
-		<div class="floor-section">
-
-		</div>
-	</div>`;
-const generateFloorAndLiftContent = function (floorCount) {
-	let string = "";
-	for (let i = 1; i <= floorCount; i++) {
-		if (i == 1) {
-			string += floorAndLiftState.floorContent1;
-		} else if (i == floorCount) {
-			string += floorContentLast;
-		} else {
-			string += floorContent;
+	const liftHTML = (function getLiftString() {
+		let string = "";
+		for (let i = 0; i < inputStatus.liftCount; i++) {
+			string += `<div class="lift" data-number=${i + 1}>lift</div>`;
 		}
-	}
-	return string;
-};
-function renderLiftsAndFloors() {
-	appViewEl.insertAdjacentHTML(
-		"beforeend",
-		generateFloorAndLiftContent(floorAndLiftState.floorCount)
-	);
-}
-simulateBtnEl.addEventListener("click", () => {
-	floorAndLiftState.floorCount = floorInputEl.value;
-	floorAndLiftState.liftCount = liftInputEl.value;
+		return string;
+	})();
 
-	floorAndLiftState.floorContent1 = `<div class="floor-element">
+	(function render() {
+		for (let i = 0; i < inputStatus.floorCount; i++) {
+			if (i + 1 == 1) {
+				appViewEl.insertAdjacentHTML(
+					"beforeend",
+					` <div class="floor-element">
                 <div>
-                    <button class="btn-up">Up</button>
+                    <button class="btn-up" data-btnNumber=${i + 1}>Up</button>
                     <hr>
                 </div>
                 <div class="floor-section">
-                ${liftContent(floorAndLiftState.liftCount)}
+               ${liftHTML}
                 </div>
-            </div>`;
-	render();
+            </div>`
+				);
+			} else if (i + 1 == inputStatus.floorCount) {
+				appViewEl.insertAdjacentHTML(
+					"beforeend",
+					`<div class="floor-element">
+                <div>
+                    <button class="btn-down" data-btnNumber=${
+											i + 1
+										}>Down</button>
+                    <hr>
+                </div>
+				<div class="floor-section"></div>
+            </div>`
+				);
+			} else {
+				appViewEl.insertAdjacentHTML(
+					"beforeend",
+					`<div class="floor-element">
+                <div>
+                    <button class="btn-up" data-btnNumber=${i + 1}>Up</button>
+                    <button class="btn-down" data-btnNumber=${
+											i + 1
+										}>Down</button>
+                    <hr>
+                </div>
+				<div class="floor-section"></div>
+            </div>`
+				);
+			}
+			console.log(i);
+		}
+	})();
 	inputViewEl.classList.add("hidden");
-});
-
-function render() {
-	renderLiftsAndFloors();
 }
+
+simulateBtnEl.addEventListener("click", setInitialStateAndRender);
